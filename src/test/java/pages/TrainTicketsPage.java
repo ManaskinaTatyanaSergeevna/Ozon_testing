be232f71348653c34151055a9640635ec807f86f
Tatyana Manaskina <bulochka_s_shokoladom@mail.ru>
2023-02-03 17:06:58 +0300

    Локаторы на странице(вкладке) "ЖД билеты". Создала локатор текста для проверки перехода на вкладку "ЖД билеты", также теперь есть кнопки переходов на вкладки "Отели", "Туры", "Авиабилеты" и их методы для клика.

---
+++ src/test/java/pages/TrainTicketsPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import com.codeborne.selenide.Selectors;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class TrainTicketsPage {
+
+    //текст для проверки перехода на страницу бронирования ЖД билетов
+    private By trainTicketsPageText = Selectors.byText("ЖД билеты на поезд");
+
+    //кнопка перехода на вкладку "Отели"
+    private By hotelsButton = Selectors.byXpath("//span[text()='Отели']");
+    //клик по кнопке "Отели"
+    public void clickOnHotelsButton(){
+        $(hotelsButton).click();
+    }
+
+    //кнопка перехода на вкладку "Туры"
+    private By toursButton = Selectors.byXpath("//span[text()='Туры']");
+    //клик по кнопке "Туры"
+    public void clickOnToursButton(){
+        $(toursButton).click();
+    }
+
+    //кнопка перехода на вкладку "Авиабилеты"
+    private By airTicketsButton = Selectors.byXpath("//span[text()='Авиабилеты']");
+    //клик по кнопке "Авиабилеты"
+    public void clickOnAirTicketsButton(){
+        $(airTicketsButton).click();
+    }
+
+    //кнопка перехода на вкладку "ЖД билеты"
+    private By trainTicketsButton = Selectors.byXpath("//span[text()='ЖД билеты']");
+    //клик по кнопке "ЖД билеты"
+    public void clickOnTrainTicketsButton(){
+        $(trainTicketsButton).click();
+    }
+}
