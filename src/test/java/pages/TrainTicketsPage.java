012c4c65b3fef450f5164d7104934b235aa31a8f
Tatyana Manaskina <bulochka_s_shokoladom@mail.ru>
2023-02-03 18:41:28 +0300

    Заполнение формы для заказа ЖД билетов. Добавила локаторы полей "Откуда", "Куда", дат "Туда" и "Обратно", кол-ва пассажиров и кнопки "Найти билеты". Создала метод для заполнения всех полей и клика на кнопку.

--- src/test/java/pages/TrainTicketsPage.java
+++ src/test/java/pages/TrainTicketsPage.java
@@ -3,6 +3,7 @@ package pages;
 import com.codeborne.selenide.Selectors;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TrainTicketsPage {
@@ -36,5 +37,54 @@ public class TrainTicketsPage {
     //клик по кнопке "ЖД билеты"
     public void clickOnTrainTicketsButton(){
         $(trainTicketsButton).click();
+    }
+    //поле "Откуда"
+    private By whereFromField = Selectors.byText("Откуда");
+    //поле "Куда"
+    private By whereToField = Selectors.byText("Куда");
+    //поле с датой "Туда"
+    private By dateThereField = Selectors.byText("Туда");
+    //поле с датой "Обратно"
+    private By dateBackField = Selectors.byText("Обратно");
+    //поле "Сколько пассажиров?"
+    private By passengersField = Selectors.byXpath("//label/div/div/div[contains(text(),'пассажир')]");
+    //кнопка "Найти билеты"
+    private By findTicketsButton = Selectors.byText("Найти билеты");
+
+    //заполнить поле "Откуда"
+    public void setWhereFromField(String fromTown){
+        $(whereFromField).setValue(fromTown);
+    }
+    //заполнить поле "Куда"
+    public void setWhereToField(String toTown){
+        $(whereToField).setValue(toTown);
+    }
+    //заполнить поле с датой "Туда"
+    public void setDateThereField(String monthTo, int dateTo){
+        $(dateThereField).click();
+        $(byXpath("//*[contains(text(),'" + monthTo + " 2023')] /../..//*[text()='" + dateTo + "']")).click();
+    }
+    //заполнить поле с датой "Обратно"
+    public void setDateBackField(String monthBack, int dateBack){
+        $(dateBackField).click();
+        $(byXpath("//*[contains(text(),'" + monthBack + " 2023')] /../..//*[text()='" + dateBack + "']")).click();
+    }
+    //заполнить поле "Сколько пассажиров?"
+    public void setPassengersField(){
+        $(passengersField).click();
+        $(Selectors.byClassName("naa6")).click();
+    }
+    //нажать на кнопку "Найти билеты"
+    public void clickOnFindTicketsButton(){
+        $(findTicketsButton).click();
+    }
+    //заполнить форму для поиска ЖД билетов
+    public void setFormFindTrainTickets(String fromTown, String toTown, String monthTo, int dateTo, String monthBack, int dateBack){
+        setWhereFromField(fromTown);
+        setWhereToField(toTown);
+        setDateThereField(monthTo, dateTo);
+        setDateBackField(monthBack, dateBack);
+        setPassengersField();
+        clickOnFindTicketsButton();
     }
 }
